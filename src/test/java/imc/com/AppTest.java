4816b55418ec2c093b3f0f7f6a0dc262a683c463
padaukwai <waimarhlaing86@gmail.com>
2025-10-16 15:47:43 +0630

    AppTest Update

--- src/test/java/imc/com/AppTest.java
+++ src/test/java/imc/com/AppTest.java
@@ -27,6 +27,13 @@ public class AppTest
         emp.title = "Engineer";
         emp.salary = 55000;
         employees.add(emp);
+        Employee emp2 = new Employee();
+        emp2.emp_no = 2;
+        emp2.first_name = "Wai";
+        emp2.last_name = "Mar";
+        emp2.title = "Teacher";
+        emp2.salary = 55000;
+        employees.add(emp2);
     }
 
     @Test
@@ -40,6 +47,19 @@ public class AppTest
         employee.salary = 55000;
         app.displayEmployee(employee);
         //testing
+    }
+    @Test
+    void displayEmplist() {
+        app.displayEmplist(employees);
+       /* int i=employees.size();
+        int index=0;
+        while(i>0)
+        {
+            System.out.print("Test Result is "+employees.get(index).first_name);
+            System.out.println(" "+employees.get(index).last_name);
+            index++;
+            i--;
+        }*/
     }
 
 
